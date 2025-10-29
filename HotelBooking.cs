b2f2396cc16866ea01d3d94725aa1086f96842e4
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2025-10-29 15:29:19 +0100

    lagt till grejer

--- HotelBooking.cs
+++ HotelBooking.cs
@@ -10,19 +10,43 @@
 
         public DateTime EndDate { get; set; }
 
+        public int Pricepernight { get; set; } = 1500;
 
-        
-        public HotelBooking(string guestname,DateTime startdate,int lenghtofstayindays)
+        public int Lenghtofstayindays { get; set; } = 0;
+
+        public int Totalinvoice { get; set; }
+
+        public bool Checkin { get; set; } = false;
+
+        public List<string> Guestwishlist = new List<string>();
+
+
+        public HotelBooking(string guestname, DateTime startdate, int lenghtofstayindays)
         {
             GuestName = guestname;
             StartDate = startdate;
+            Lenghtofstayindays = lenghtofstayindays;
             EndDate = startdate.AddDays(lenghtofstayindays);
+            Totalinvoice = Lenghtofstayindays * Pricepernight;
 
         }
 
         public void PrintInfo()
         {
-            Console.WriteLine($"{GuestName}\nAnkomst:{StartDate.ToShortDateString()}\nAvresa:{EndDate.ToShortDateString()}");
+           
+            Console.WriteLine($"{GuestName}\nAnkomst:{StartDate.ToShortDateString()}\n" +
+                $"Avresa:{EndDate.ToShortDateString()}\nTotalpris:{Totalinvoice}");
+        }
+
+        public void AddToWishlist()
+        {
+            Console.WriteLine("Har du något särskillt önskemål? Y/N");
+
+           
+
+            string Wish=Console.ReadLine();
+            Guestwishlist.Add(Wish);
+
         }
 
     }
